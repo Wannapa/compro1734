bed5b2cc54f95136150d1091f5aa5fac43102f42
Wannapa <milk_badgril@hotmail.com>
2012-06-21 12:28:12 +0700

    เพิ่ม  "Programming is fun"

--- src/th/ac/nu/students/u54341734/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/students/u54341734/compro/ch01/HelloWorld.java
@@ -4,7 +4,7 @@ package th.ac.nu.students.u54341734.compro.ch01;
 public class HelloWorld {
     public static void main(String[]args){
         System.out.println("Hello,World");
-        
+        System.out.println("Programming is fun");
     }
     
 }
